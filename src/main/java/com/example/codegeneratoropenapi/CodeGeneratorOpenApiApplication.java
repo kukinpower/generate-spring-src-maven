4e41e2b39249b4342e5eeb5965d15173a8eb4ec1
kukinpower <thekukinpower@gmail.com>
2021-11-27 23:24:15 +0300

    add all files to generate src

---
+++ src/main/java/com/example/codegeneratoropenapi/CodeGeneratorOpenApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.codegeneratoropenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGeneratorOpenApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeGeneratorOpenApiApplication.class, args);
+  }
+}
